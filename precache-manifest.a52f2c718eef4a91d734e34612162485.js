0d06e9e13b39fc4fdca921d465e1bd2ff45d4ba4
Champkinz <danu_lak@live.com>
2020-04-07 23:06:05 +0530

    Updates

---
+++ precache-manifest.a52f2c718eef4a91d734e34612162485.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "45d937aca1af646f04425b44665cfcde",
+    "url": "/covid19-srilanka-helper/index.html"
+  },
+  {
+    "revision": "0e4607d0a0a7b21ff095",
+    "url": "/covid19-srilanka-helper/static/css/main.0c7fa0dd.chunk.css"
+  },
+  {
+    "revision": "0daca1de0810cdbb82aa",
+    "url": "/covid19-srilanka-helper/static/js/2.e1bf112a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/covid19-srilanka-helper/static/js/2.e1bf112a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0e4607d0a0a7b21ff095",
+    "url": "/covid19-srilanka-helper/static/js/main.e15bb0fb.chunk.js"
+  },
+  {
+    "revision": "3110cdced9ccf368036c",
+    "url": "/covid19-srilanka-helper/static/js/runtime-main.24fac9e5.js"
+  }
+]);
