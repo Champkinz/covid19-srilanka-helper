6f88f7d6b014251f5fa100bae494e4a907c5e2ee
Charana Amarasekara <danu_lak@live.com>
2020-04-13 22:25:44 +0530

    Updates

--- precache-manifest.aa78f35f75f9827c9020f2fdfbad940d.js
+++ precache-manifest.aa78f35f75f9827c9020f2fdfbad940d.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "b2529d2f570d8ec989a9fcdb7a48f843",
+    "revision": "2444195619746f02365d9a3f7fff402a",
     "url": "/covid19-srilanka-helper/index.html"
   },
   {
-    "revision": "6314934b543c2701e24c",
+    "revision": "596a1a2271eda9e078d0",
     "url": "/covid19-srilanka-helper/static/css/main.0c7fa0dd.chunk.css"
   },
   {
-    "revision": "b309fb4e1d341e62c020",
-    "url": "/covid19-srilanka-helper/static/js/2.3a8a814f.chunk.js"
+    "revision": "83707c6d1fb46650fac6",
+    "url": "/covid19-srilanka-helper/static/js/2.cf39558e.chunk.js"
   },
   {
     "revision": "3453b8997016469371284a28c0e873e2",
-    "url": "/covid19-srilanka-helper/static/js/2.3a8a814f.chunk.js.LICENSE.txt"
+    "url": "/covid19-srilanka-helper/static/js/2.cf39558e.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "6314934b543c2701e24c",
-    "url": "/covid19-srilanka-helper/static/js/main.cd877085.chunk.js"
+    "revision": "596a1a2271eda9e078d0",
+    "url": "/covid19-srilanka-helper/static/js/main.72e622ae.chunk.js"
   },
   {
     "revision": "3110cdced9ccf368036c",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/covid19-srilanka-helper/precache-manifest.d123c88da998b180acc77ca26f50fff1.js"
+  "/covid19-srilanka-helper/precache-manifest.aa78f35f75f9827c9020f2fdfbad940d.js"
 );
 
 self.addEventListener('message', (event) => {
