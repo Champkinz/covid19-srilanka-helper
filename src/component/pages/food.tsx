8bb67e88b282745b4e223f56659c501909c6daec
Charana Amarasekara <danu_lak@live.com>
2020-05-20 12:06:03 +0530

    new

--- src/component/pages/food.tsx
+++ src/component/pages/food.tsx
@@ -1,9 +1,11 @@
 import React, { useState, useEffect } from "react";
 import Block from "./block";
 import { shops } from "../data/shops";
+import Modal from "react-modal";
 
 function Food() {
   const [data, setData] = useState(shops);
+  const [modal, setModal] = useState(false);
 
   useEffect(() => {
     setData(shops);
@@ -25,6 +27,25 @@ function Food() {
 
   return (
     <>
+      <Modal
+        isOpen={modal}
+        onRequestClose={() => setModal(false)}
+        contentLabel="Example Modal"
+      >
+        <div className="flex flex-1 flex-col h-full items-center justify-center">
+          <p style={{ fontSize: 24, fontWeight: "bold" }}>
+            Please email us the business details to charana.am@gmail.com to add
+          </p>
+        </div>
+      </Modal>
+      <div className="flex flex-1 flex-row justify-end px-6">
+        <div
+          onClick={() => setModal(true)}
+          className="bg-blue-400 mt-2 px-2 py-1 rounded-full text-white font-bold cursor-pointer"
+        >
+          <p>Click here to add your business</p>
+        </div>
+      </div>
       <input
         className="bg-white focus:outline-none focus:shadow-outline border border-gray-300 rounded-lg py-2 px-4 mx-6 mt-6 appearance-none leading-normal"
         placeholder="Enter Delivery Location"
